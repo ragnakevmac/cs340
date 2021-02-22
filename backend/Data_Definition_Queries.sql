d09e5d3f1b65822f73c88c479355115b00373696
Kevin Macandog <kevinmagnus_001@yahoo.com>
2021-02-22 08:16:08 -0800

    Commuter_Passes now take Email, not ID, for INSERT

--- backend/Data_Definition_Queries.sql
+++ backend/Data_Definition_Queries.sql
@@ -204,9 +204,9 @@ INSERT INTO `Passengers` (`first_name`, `last_name`, `birthdate`, `occupation`, 
 --
 
 INSERT INTO `Commuter_Passes` (`cost`, `start_date`, `end_date`, `passenger_id`, `trainline_id`) VALUES
-('100.00', '2020-01-10', '2021-11-11', (SELECT passenger_id FROM Passengers WHERE email = "Risa_Oribe@gmail.com"), (SELECT trainline_id FROM Trainlines WHERE trainline_company = "Yamanote Line")),
-('90.00', '2021-01-11', '2021-05-25', (SELECT passenger_id FROM Passengers WHERE email = "Risa_Oribe@gmail.com"), (SELECT trainline_id FROM Trainlines WHERE trainline_company = "Ginza Line")),
-('150.00', '2021-01-20', '2021-04-20', (SELECT passenger_id FROM Passengers WHERE email = "Risa_Oribe@gmail.com"), (SELECT trainline_id FROM Trainlines WHERE trainline_company = "Hibiya Line")),
+('100.00', '2020-01-10', '2021-11-11', (SELECT passenger_id FROM Passengers WHERE email = "Risa_Oribe@pmail.com"), (SELECT trainline_id FROM Trainlines WHERE trainline_company = "Yamanote Line")),
+('90.00', '2021-01-11', '2021-05-25', (SELECT passenger_id FROM Passengers WHERE email = "Risa_Oribe@pmail.com"), (SELECT trainline_id FROM Trainlines WHERE trainline_company = "Ginza Line")),
+('150.00', '2021-01-20', '2021-04-20', (SELECT passenger_id FROM Passengers WHERE email = "Risa_Oribe@pmail.com"), (SELECT trainline_id FROM Trainlines WHERE trainline_company = "Hibiya Line")),
 ('0.00', '2021-02-11', '2031-02-11', NULL, (SELECT trainline_id FROM Trainlines WHERE trainline_company = "Yamanote Line")),
 ('0.00', '2021-02-11', '2031-02-11', NULL, (SELECT trainline_id FROM Trainlines WHERE trainline_company = "Ginza Line")),
 ('0.00', '2021-01-11', '2021-04-11', NULL, (SELECT trainline_id FROM Trainlines WHERE trainline_company = "Hibiya Line")),
